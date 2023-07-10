ea331bfe3e9068d358381284d402414e5980336b
Süleyman Üren <suleymanuren07@gmail.com>
2023-07-10 16:33:14 +0300

    ADD app build

---
+++ Uber/Uber/UberApp.swift
@@ -0,0 +1,18 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Bulut Sistem on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
